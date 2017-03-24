19e47ed2e4bee5324a25460790d56fccfc1a08a3
Scott Lessans (the brain) <s@nourish.ai>
2017-03-24 14:26:07 -0700

    fixed carthage build

---
+++ Sources/RingBuffer.h
@@ -0,0 +1,19 @@
+//
+//  RingBuffer.h
+//  RingBuffer
+//
+//  Created by The Brain on 3/24/17.
+//  Copyright © 2017 Nourish Technology, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RingBuffer.
+FOUNDATION_EXPORT double RingBufferVersionNumber;
+
+//! Project version string for RingBuffer.
+FOUNDATION_EXPORT const unsigned char RingBufferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RingBuffer/PublicHeader.h>
+
+
